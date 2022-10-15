4c23eadedc7a464f6cd92b432359db8e64d1206a
Tyla Jeffs <tylajeffs@gmail.com>
2022-10-15 00:56:00 +0000

    add nav bar

--- app/helpers/sites_helper.rb
+++ app/helpers/sites_helper.rb
@@ -1,2 +1,20 @@
 module SitesHelper
+    
+    
+    def nav_link(text,path)
+        if current_page?(path)
+            content_tag :li, class: "nav-item" do
+                link_to(text, path, class: "nav-link active", aria: { current: 'page'})
+            end
+        else 
+            content_tag :li, class: "nav-items" do
+                link_to(text, path, class: 'nav-link')
+            end
+        end
+    end
+            
+    
+    
+    
+    
 end
