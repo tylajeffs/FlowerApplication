ff065870f62d69a74cece97e351c74de07e7c985
Tyla Jeffs <tylajeffs@gmail.com>
2022-10-19 04:34:41 +0000

    add database validations

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,2 +1,4 @@
 class Review < ApplicationRecord
+    validates :title, presence: true
+    validates :stars, presence: true, :inclusion => {:in => 0..5, :message => "Value should be between 0 and 5"}
 end
