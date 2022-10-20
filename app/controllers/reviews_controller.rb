1c8d01ca9f72d300a2307f20bcd6ef3e89dfdf8b
Tyla Jeffs <tylajeffs@gmail.com>
2022-10-20 05:19:11 +0000

    add create new review functionality

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,6 @@
 class ReviewsController < ApplicationController
   before_action :load_package
+  before_action :load_review, except: [:index,:new,:create]
   
   def index
   end
@@ -9,10 +10,36 @@ class ReviewsController < ApplicationController
     @review = Review.new
   end
   
+  def create
+    @review = Review.new review_params
+    if @package.reviews << @review
+      redirect_to package_review_path(@package,@review), notice: "Review Created"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+  
+  
+  def show
+  end
+  
+  
+  
+  
+  
   
   
   
   private
+  
+  def review_params
+    params.require(:review).permit(:title, :stars, :body)
+  end
+  
+  
+  def load_review
+    @review = Review.find params[:id]
+  end
   
   
   def load_package
