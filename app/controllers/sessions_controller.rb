060ac94bd1ffa5b5832a9e254bf3e924690b754b
Tyla Jeffs <tylajeffs@gmail.com>
2022-11-05 21:18:40 +0000

    add log in functionality and store user session in cookie

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,19 @@
 class SessionsController < ApplicationController
   def new
   end
+  
+  
+  def create
+    @user = User.where(email: params[:email]).first
+    if @user && @user.authenticate(params[:password])
+      #login
+      session[:user_id] = @user.id
+      redirect_to root_path, notice: "Logged in!" 
+    else 
+      redirect_to login_path, alert: "Invalid email or password"
+    end
+  end
+  
+  
+  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ApplicationRecord
     has_secure_password
     
     validates :name, presence: :true
-    validates :email, format: { with: /@/, message: "must have an @" }
+    validates :email, format: { with: /@/, message: "must have an @" }, uniqueness: :true
 end
