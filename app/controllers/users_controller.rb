828386bf47ae17c0289060e584e7cfdcc5e50a54
Tyla Jeffs <tylajeffs@gmail.com>
2022-11-05 21:06:53 +0000

    add sign up page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,28 @@
 class UsersController < ApplicationController
+  
   def new
+    @user = User.new
   end
+  
+  def create
+    @user = User.new user_params
+    if @user.save
+      redirect_to root_path, notice: "You've successfully signed up!"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+  
+  
+  
+  
+  
+  private
+  
+  
+  def user_params
+    params.require(:user).permit(:name,:email,:password,:password_confirmation)
+  end
+  
+  
 end
