9d71be1d16899bfd76579031cd2f3ef3f65a87fd
Tyla Jeffs <tylajeffs@gmail.com>
2022-11-05 20:38:55 +0000

    create new user model and database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_19_042903) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_05_203551) do
   create_table "packages", force: :cascade do |t|
     t.string "name"
     t.text "description"
@@ -24,6 +24,14 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_19_042903) do
     t.text "body"
     t.integer "stars"
     t.integer "package_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
