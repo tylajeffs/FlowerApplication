55b5ad2b93a9ec7ac455fb84d9cf6e6e146075a7
Tyla Jeffs <tylajeffs@gmail.com>
2022-11-11 20:01:01 +0000

    add user hash

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -1,7 +1,7 @@
 class AuthenticationsController < ApplicationController
     
     def create
-        render json: auth_hash
+        render json: user_hash
     end
     
     
@@ -11,7 +11,21 @@ class AuthenticationsController < ApplicationController
     
     def auth_hash
         request.env['omniauth.auth']
-        
+    end
+    
+    def user_hash
+        hash = {}
+        hash[:uid] = auth_hash['uid']
+        hash[:provider] = auth_hash['provider']
+        if auth_hash['info']
+            hash[:name] = auth_hash['info']['name']
+            hash[:email] = auth_hash['info']['email']
+        end
+        if auth_hash['credentials']
+            hash[:token] = auth_hash['credentials']['token']
+        end
+        hash
+            
     end
     
 end
