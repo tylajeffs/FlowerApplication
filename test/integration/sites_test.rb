1061598b58eed78b71318347b54782d011609364
Tyla Jeffs <tylajeffs@gmail.com>
2022-11-30 23:24:35 +0000

    add tests for homepage and about page

--- test/integration/sites_test.rb
+++ test/integration/sites_test.rb
@@ -1,7 +1,15 @@
 require "test_helper"
 
 class SitesTest < ActionDispatch::IntegrationTest
-   test "the truth" do
-     assert true
+   test "home page works" do
+     visit root_path
+     
+     assert_selector "h1", text: "The Flower Shop"
+   end
+   
+   test "about page works" do
+     visit about_path
+     
+     assert_selector "h1", text: "About"
    end
 end
