e92acb69b782ca16c15a550432b6fc95c94346a6
Tyla Jeffs <tylajeffs@gmail.com>
2022-12-01 00:58:56 +0000

    add login_user method

--- test/support/authentication_macros.rb
+++ test/support/authentication_macros.rb
@@ -1,5 +1,18 @@
 module AuthenticationMacros
     
+    def login_user(user_properties = {})
+        user = FactoryBot.create :user, user_properties
+        visit login_path
+        
+        fill_in "Email", with: user.email
+        fill_in "Password", with: user.password
+        
+        click_button "Log in"
+        
+        user
+        
+    end
+    
     def reset_login
         visit logout_path
     end
