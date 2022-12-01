5366023a1ff2b0116a431ee3c5457a20ed0c691d
Tyla Jeffs <tylajeffs@gmail.com>
2022-12-01 00:50:44 +0000

    set up user login test

--- test/integration/sessions_test.rb
+++ test/integration/sessions_test.rb
@@ -2,7 +2,20 @@ require "test_helper"
 
 class SessionsTest < ActionDispatch::IntegrationTest
    test "users can login" do
-     assert true
+     user = FactoryBot.create :user, email: "testlogin@test.com", password: "secret"
+     
+     visit root_path
+     
+     click_on "Login"
+     
+     fill_in "Email", with: "testlogin@test.com"
+     fill_in "Password", with: "secret"
+    
+     click_button "Log in"
+     
+     assert_equal page.current_path, root_path
+     assert_text "Log Out"
+     refute page.has_content?("Login")
    end
    
    test "users can't login with the wrong password" do
